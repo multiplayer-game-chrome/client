2f883787b5bca1130cb172da8491944b43e4d273
ryan <ryanmaulanaputra@gmail.com>
2020-03-20 00:17:09 +0700

    added notification for win and lose condition

--- src/store/index.js
+++ src/store/index.js
@@ -7,11 +7,15 @@ export default new Vuex.Store({
   state: {
     boardLength: 9,
     mark: 'X',
-    board: []
+    board: [],
+    winner: ''
   },
   mutations: {
     SET_BOARD (state, payload) {
       state.board = payload
+    },
+    SET_WINNER (state, payload) {
+      state.winner = payload
     }
   },
   actions: {},
