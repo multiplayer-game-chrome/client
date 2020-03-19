3c4fdff758bf1ef6db79597474eb037bff5cbcd7
ryan <ryanmaulanaputra@gmail.com>
2020-03-19 21:35:12 +0700

    server and client board state connection done

--- src/components/GameTile.vue
+++ src/components/GameTile.vue
@@ -2,23 +2,31 @@
   <div class="col-4 px-0" id="tictactoe-tile" @click="sendCoordinate">
     <div class="card">
       <div class="card-body">
-        <h1>[{{ x }} , {{ y }}]</h1>
+        <h1>{{value}}</h1>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'GameTile',
-  props: ['index'],
+  props: ['index', 'content'],
+  data () {
+    return {
+      value: ''
+    }
+  },
   computed: {
     x: function () {
       return this.index % 3
     },
     y: function () {
       return Math.floor(this.index / 3)
-    }
+    },
+    ...mapState(['board'])
   },
   methods: {
     sendCoordinate () {
@@ -27,6 +35,12 @@ export default {
         x: this.x,
         y: this.y
       })
+    }
+  },
+  watch: {
+    board () {
+      const val = this.board[this.x][this.y]
+      this.value = val === 'z' ? '' : val
     }
   }
 }
